523b106e32386df570357b815d09f121f32d557c
Nastya <nastya@example.com>
2018-05-02 01:31:11 +0300

    pictures and script have been changed

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -246,36 +246,6 @@ CREATE TABLE IF NOT EXISTS customer_bouquet_order (
 	DEFAULT CHARACTER SET = utf8;
 
 
-INSERT INTO flower (species, sort, color, length, amount, price, picture)
-VALUES ('Роза', 'Freedom', 'Красный', 60, 70, 4.6, 'freedom_flower.jpg');
-
-INSERT INTO flower (species, sort, color, length, amount, price, picture)
-VALUES ('Роза', 'Talea', 'Нежно бежевый', 50, 70, 2.5, 'talea_flower.jpg');
-
-INSERT INTO flower (species, sort, color, length, amount, price, picture)
-VALUES ('Роза', 'Penny lane', 'Жёлтый', 50, 60, 2.9, 'penny_lane_flower.jpg');
-
-INSERT INTO flower (species, sort, color, length, amount, price, picture)
-VALUES ('Роза', 'Avalanche', 'Белый', 50, 60, 2.9, 'avalanche_flower.jpg');
-
-INSERT INTO flower (species, sort, color, length, amount, price, picture)
-VALUES ('Роза', 'Aqua', 'Розовый', 50, 80, 3.6, 'aqua_flower.jpg');
-
-INSERT INTO flower (species, sort, color, length, amount, price, picture)
-VALUES ('Роза', 'Red Naomi', 'Красный', 80, 50, 4.5, 'red_naomi_flower.jpg');
-
-INSERT INTO flower (species, sort, color, length, amount, price, picture)
-VALUES ('Роза', 'Jumilia', 'Белый', 70, 60, 4.2, 'jumilia_flower.jpg');
-
-INSERT INTO flower (species, sort, color, length, amount, price, picture)
-VALUES ('Роза', 'Wow', 'Оранжевый', 70, 50, 4.4, 'wow_flower.jpg');
-
-INSERT INTO flower (species, sort, color, length, amount, price, picture)
-VALUES ('Роза', ' Sweet Avalanche', 'Нежно розовый', 50, 50, 4.4, 'sweet_avalanche_flower.jpg');
-
-INSERT INTO flower (species, sort, color, length, amount, price, picture)
-VALUES ('Роза', ' Senorita ', 'Нежно розовый', 70, 60, 4, 'senorita_flower.jpg');
-
 
 
 INSERT INTO user (login, password, status)
@@ -290,22 +260,164 @@ VALUES ('Людмила', 'Иванова', '+375 29 333-22-22');
 INSERT INTO address (city, street, house, block, flat)
 VALUES ('Минск', 'ул. Якуба Колоса', 56, 0, 4);
 
+
+
 INSERT INTO flower (species, sort, color, length, amount, price, picture)
-VALUES ('Тюльпан', 'Barcelona', 'Розовый', 25, 60, 2.1, 'tulip_barcelona.jpg');
+VALUES ('Лилия', ' Cogoleto', 'Розовый', 90, 40, 6.9, 'lily_cogoleto.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Лилия', ' Mapira', 'Бордовый', 95, 40, 7.5, 'lily_mapira.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Лилия', ' Corsage', 'Нежно-розовый', 80, 60, 6.5, 'lily_corsage.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Роза', 'Red Naomi', 'Красный', 80, 50, 4.5, 'rose_red_naomi.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Роза', 'Aqua', 'Розовый', 50, 80, 3.6, 'rose_aqua.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Роза', 'Avalanche', 'Белый', 50, 60, 2.9, 'rose_avalanche.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Роза', ' Sweet Avalanche', 'Нежно розовый', 50, 50, 4.4, 'rose_sweet_avalanche.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Роза', 'Jumilia', 'Белый', 70, 60, 4.2, 'rose_jumilia.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Роза', 'Wow', 'Оранжевый', 70, 50, 4.4, 'rose_wow.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Роза', 'Talea', 'Нежно бежевый', 50, 70, 2.5, 'rose_talea.jpg');
+
+/*INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Роза', 'Freedom', 'Красный', 60, 70, 4.6, 'freedom_flower.jpg');*/
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Роза', 'Penny lane', 'Жёлтый', 50, 60, 2.9, 'rose_penny_lane.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Роза', ' Senorita ', 'Нежно розовый', 70, 60, 4, 'rose_senorita.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Тюльпан', ' Christmas Dream', 'Розовый', 40, 60, 2.1, 'tulip_christmas_dream.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Тюльпан', 'Alibi', 'Фиолетовый', 30, 70, 2.3, 'tulip_alibi.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Тюльпан', 'Furand', 'Красный', 35, 50, 2.3, 'tulip_furand.jpg');
+
+INSERT INTO flower (species, sort, color, length, amount, price, picture)
+VALUES ('Тюльпан', 'Strong Gold', 'Жёлтый', 45, 60, 1.9, 'tulip_strong_gold.jpg');
+
+
+
 
 INSERT INTO decoration (name, amount, price, picture)
-VALUES ('Бабочка', 60, 2.6, null);
+VALUES ('Веточка вербная (залёная), 80 см', 200, 2.4, 'vetka5.jpg');
+
+INSERT INTO decoration (name, amount, price, picture)
+VALUES ('Веточка вербная (белая), 80 см', 200, 2.2, 'vetka1.jpg');
+
+INSERT INTO decoration (name, amount, price, picture)
+VALUES ('Ветка сакуры, 82 см', 200, 12.3, 'vetka3.jpg ');
+
+INSERT INTO decoration (name, amount, price, picture)
+VALUES ('Рускус, 70 см', 200, 8.6, 'vetka4.jpg');
+
+INSERT INTO decoration (name, amount, price, picture)
+VALUES ('Веточка искусственная, 52 см', 200, 3.8, 'vetka2.jpg ');
+
+INSERT INTO decoration (name, amount, price, picture)
+VALUES ('Искусственный красный цветок, 40 см', 100, 4.9, 'cvetok3.jpg');
+
+INSERT INTO decoration (name, amount, price, picture)
+VALUES ('Искусственный розовый цветок, 50 см', 100, 4.9, 'cvetok5.jpg');
+
+
 
 INSERT INTO pack (name, amount, price, picture)
-VALUES ('Бумага флористическая двусторонняя, 50 см', 100, 2, null);
+VALUES ('Бумага флористическая двусторонняя, 50 см', 100, 2, 'pack1.jpg');
+
+INSERT INTO pack (name, amount, price, picture)
+VALUES ('Плёнка, 70 см', 200, 3.2, 'plenka3.jpg');
+
+INSERT INTO pack (name, amount, price, picture)
+VALUES ('Коробка круглая, высота – 45 см', 200, 5.9, 'korobka2.jpg');
+
+INSERT INTO pack (name, amount, price, picture)
+VALUES ('Сизал, 60 см', 200, 3.5, 'sizal1.jpg');
+
+INSERT INTO pack (name, amount, price, picture)
+VALUES ('Коробка квадратная, высота – 30 см', 180, 5.1, 'korobka1.jpg');
+
+INSERT INTO pack (name, amount, price, picture)
+VALUES ('Корзина, ширина – 50 см', 100, 6.8, 'korzina2.jpg');
+
+INSERT INTO pack (name, amount, price, picture)
+VALUES ('Органза, 60 см', 100, 3, 'organza1.jpg');
+
+
+
+INSERT INTO bouquet (name, composition, height, diameter, weight, amount, price, picture)
+VALUES ('Яркий букет из тюльпанов', '71 тюльпан, жёлтая и красная лента',
+				50, 50, 550, 30, 69.7, 'bouquet_bright.jpg');
+
+INSERT INTO bouquet (name, composition, height, diameter, weight, amount, price, picture)
+VALUES ('Нежный букет из тюльпанов', '25 тюльпанов, бежевый сизаль, белая атласная лента',
+				35, 40, 400, 50, 22.4, 'bouquet_delicate.jpg');
 
 INSERT INTO bouquet (name, composition, height, diameter, weight, amount, price, picture)
 VALUES ('Букет из роз Avalanche', '30 роз Avalanche, ветки рускуса, белая лента',
-				60, 30, 500, 30, 54.99, null);
+				60, 40, 500, 30, 44.99, 'bouquet_avalanche.jpg');
+
+INSERT INTO bouquet (name, composition, height, diameter, weight, amount, price, picture)
+VALUES ('Букет из роз Red Naomi', '50 роз Red Naomi, красная лента',
+				70, 50, 600, 25, 67.5, 'bouquet_red_naomi.jpg');
+
+INSERT INTO bouquet (name, composition, height, diameter, weight, amount, price, picture)
+VALUES ('Букет из роз Jumilia', '45 роз Jumilia, розовая лента',
+				50, 50, 450, 35, 55.3, 'bouquet_jumilia.jpg');
+
+INSERT INTO bouquet (name, composition, height, diameter, weight, amount, price, picture)
+VALUES ('Букет из роз Senorita', '35 роз Senorita, розовая лента',
+				50, 40, 450, 30, 32.99, 'bouquet_senorita.jpg');
+
+INSERT INTO bouquet (name, composition, height, diameter, weight, amount, price, picture)
+VALUES ('Букет из тюльпанов Christmas Dream ', '40 тюльпанов Christmas Dream, белая лента',
+				50, 40, 450, 50, 39.99, 'bouquet_christmas_dream.jpg');
+
+INSERT INTO bouquet (name, composition, height, diameter, weight, amount, price, picture)
+VALUES ('Букет из тюльпанов White Dream', '25 тюльпанов White Dream, белая лента',
+				45, 30, 380, 50, 23.5, 'bouquet_white_dream.jpg');
+
+INSERT INTO bouquet (name, composition, height, diameter, weight, amount, price, picture)
+VALUES ('Букет с лилиями и ирисами', '7 лилий, 5 ирисов, 3 розы, декоративные украшения, розовый сизаль, белая атласная лента',
+				45, 40, 500, 20, 65.99, 'bouquet_lily_iris.jpg');
+
+
 
 INSERT INTO composition (name, composition, pack, amount, price, picture)
-VALUES ('Нежное утро', '20 роз Senorita, 12 ромашек, ветки рускуса, мелкие цветы',
-				'Круглая фиолетовая коробка в горошек', 10, 48.89, null);
+VALUES ('Сказка', '20 роз Senorita, 12 ромашек, ветки рускуса, мелкие цветы',
+				'Круглая фиолетовая коробка в горошек', 10, 48.89, ' composition_rose_chamomile.jpg');
+
+INSERT INTO composition (name, composition, pack, amount, price, picture)
+VALUES ('Нежное утро', '40 тюльпанов Christmas Dream, широкая розовая атласная лента',
+				'Круглая фиолетовая коробка', 20, 38.99, ' composition_tulip.jpg');
+
+INSERT INTO composition (name, composition, pack, amount, price, picture)
+VALUES ('Сюрприз', '9 роз, 3 тюльпана, декоративные украшения, розовая атласная лента',
+				'Квадратная коробка', 40, 18.99, ' composition_tulip_rose.jpg');
+
+INSERT INTO composition (name, composition, pack, amount, price, picture)
+VALUES ('Живая композиция', '2 герберы, 3 розы, декоративные украшения, широкая розовая атласная лента',
+				'Плетёная корзина', 20, 50.2, ' composition_gerbera_rose.jpg');
+
+
+
 
 INSERT INTO customer_bouquet (pack_id)
 VALUES (1);
